47ffbdc185837aad793bcc2c8f6cb1c1a850351e
Luna Borella <ulfrthewolf@gmail.com>
2023-03-25 23:21:17 +1000

    created the bind group. dababy.jpg.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use image::GenericImageView;
 use wgpu::{
     BlendState, Color, ColorTargetState, ColorWrites, CommandEncoderDescriptor, Device, Face,
     FragmentState, FrontFace, Instance, InstanceDescriptor, MultisampleState,
@@ -35,6 +36,7 @@ struct State {
     clear_colour: Color,
     changing_clear_colour: bool,
     cursor_position: Option<PhysicalPosition<f64>>,
+    diffuse_bind_group: wgpu::BindGroup,
 }
 
 const WIDTH: u32 = 800;
@@ -176,6 +178,119 @@ impl State {
         };
         surface.configure(&device, &config);
 
+        let diffuse_bytes = include_bytes!("../assets/dababy-256.jpg");
+        let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
+        let diffuse_rgba = diffuse_image.to_rgba8();
+
+        let dimensions = diffuse_image.dimensions();
+
+        let texture_size = wgpu::Extent3d {
+            width: dimensions.0,
+            height: dimensions.1,
+            depth_or_array_layers: 1,
+        };
+
+        let diffuse_texture = device.create_texture(
+            &wgpu::TextureDescriptor {
+                // All textures are stored as 3D, we represent our 2D texture
+                // by setting depth to 1
+                size: texture_size,
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                // Most images are stored using sRGB
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                // TEXTURE_BINDING tells wgpu that we want to use this texture in shaders
+                // COPY_DST means we want to copy data to this texture
+                usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+                label: Some("diffuse_texture"),
+
+                view_formats: &[],
+            }
+        );
+
+        // Now we have to get our texture data into the texture
+        queue.write_texture(
+            // Where to copy the pixel data
+            wgpu::ImageCopyTexture {
+                texture: &diffuse_texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+                aspect: wgpu::TextureAspect::All,
+            },
+            // The actual data
+            &diffuse_rgba,
+            // The layout of the texture
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: std::num::NonZeroU32::new(4 * dimensions.0),
+                rows_per_image: std::num::NonZeroU32::new(dimensions.1),
+            },
+            texture_size
+        );
+
+        // The texture view is not so important right now so just use default
+        let diffuse_texture_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
+        // The sampler tells wgpu how to figure out what colour to set
+        // each pixel when rendering.
+        let diffuse_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        });
+
+        // A BindGroup describes a set of resources and how they can be
+        // accessed by the shader. We start by defining the layout of it.
+        let texture_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Texture {
+                        multisampled: false,
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        sample_type: wgpu::TextureSampleType::Float { 
+                            filterable: true 
+                        },
+                    },
+                    count: None,
+                },
+
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    // This should match the filterable field
+                    // of the corresponding Texture entry above
+                    ty: wgpu::BindingType::Sampler(
+                        wgpu::SamplerBindingType::Filtering
+                    ),
+                    count: None,
+                }
+            ],
+            label: Some("texture_bind_group_layout"),
+        });
+
+        let diffuse_bind_group = device.create_bind_group(
+            &wgpu::BindGroupDescriptor {
+                layout: &texture_bind_group_layout,
+                entries: &[
+                    wgpu::BindGroupEntry {
+                        binding: 0,
+                        resource: wgpu::BindingResource::TextureView(&diffuse_texture_view)
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 1,
+                        resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
+                    }
+                ],
+                label: Some("diffuse_bind_group"),
+            }
+        );
+
         let shader = device.create_shader_module(ShaderModuleDescriptor {
             label: Some("Shader"),
             source: wgpu::ShaderSource::Wgsl(include_str!("shader1.wgsl").into()),
@@ -235,6 +350,7 @@ impl State {
             config,
             size,
             render_pipeline,
+            diffuse_bind_group,
             clear_colour: CLEAR_COLOUR,
             changing_clear_colour: false,
             cursor_position: None,
